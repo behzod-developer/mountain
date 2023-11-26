08680359cb8329a915986b8d69ab062b790cfa51
Behzod <behzodsharifjonov048@gmail.com>
2023-11-26 01:19:52 -0800

    secend

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import Logo from '../../assets/Logo.png'
+import { FaTelegram } from "react-icons/fa6";
+import { IoLogoWhatsapp } from "react-icons/io";
+import { FaYoutube } from "react-icons/fa";
+import {NavLink} from "react-router-dom"
+import "./Navbar.css"
+
+function Navbar() {
+    return (
+        <nav>
+            <ul>
+                <li>
+                    <img src={Logo} alt="" />
+                </li>
+                <li>
+                    <NavLink>О нас</NavLink>
+                    <NavLink>Услуги</NavLink>
+                    <NavLink>Как купить</NavLink>
+                    <NavLink>Преимущества</NavLink>
+                    <NavLink>Отзывы</NavLink>
+                    <NavLink>Контакты</NavLink>
+                </li>
+                <li>
+                    <FaTelegram />
+                    <IoLogoWhatsapp />
+                    <FaYoutube />
+                </li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Navbar
