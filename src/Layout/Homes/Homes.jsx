f5cbb81c18f999a22d97871b51ef5926718456c7
Behzod <behzodsharifjonov048@gmail.com>
2023-11-27 04:36:30 -0800

    about qoshildi

--- src/Layout/Homes/Homes.jsx
+++ src/Layout/Homes/Homes.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import About from '../../Pages/About/About'
 import Home from '../../Pages/Home/Home'
 
 function Homes() {
   return (
         <main>
             <Home />
+            <About />
         </main>
   )
 }
--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -1,9 +1,36 @@
 import React from 'react'
+import card1 from "../../assets/card1.png"
+import "./About.css"
 
 function About() {
-  return (
-    <div>About</div>
-  )
+    return (
+        <section className='about'>
+            <div className="container">
+                <div className="parent">
+                    <h1>Поможем вам выгодно купить и привезти автомобиль в любую точку России</h1>
+                    <p>Предлагаем Вам купить автомобиль дешевле и лучше, но за тот же бюджет</p>
+                    <button>Оставить заявку</button>
+                    <div className="cards">
+                        <div className="card">
+                            <h2>С пробегом</h2>
+                            <span>Предлагаем купить автомобиль дешевле до 60%, от рыночной цены на Авито и Авто.ру</span>
+                            <img src={card1} alt="" />
+                        </div>
+                        <div className="card">
+                            <h2>С пробегом</h2>
+                            <span>Предлагаем купить автомобиль дешевле до 60%, от рыночной цены на Авито и Авто.ру</span>
+                            <img src={card1} alt="" />
+                        </div>
+                        <div className="card">
+                            <h2>С пробегом</h2>
+                            <span>Предлагаем купить автомобиль дешевле до 60%, от рыночной цены на Авито и Авто.ру</span>
+                            <img src={card1} alt="" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
 }
 
 export default About
